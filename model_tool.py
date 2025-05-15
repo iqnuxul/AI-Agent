ddb8aa89e7b9af6d5c6bc36556094cdf176da9c4
lulu <mluxunqi@gmail.com>
2025-05-15 10:49:29 +0200

    changes
    changes

--- model_tool.py
+++ model_tool.py
@@ -7,14 +7,15 @@ import json
 class MLP(nn.Module):
     def __init__(self, input_size):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_size, 200)
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.dropout = nn.Dropout(0.1)
+        self.fc3 = nn.Linear(64, 1)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(200, 50)
-        self.fc3 = nn.Linear(50, 1)
 
     def forward(self, x):
         x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
+        x = self.dropout(self.relu(self.fc2(x)))
         return self.fc3(x)
 
 
